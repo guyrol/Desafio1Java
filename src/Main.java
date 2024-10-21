eba9b79d22d85f3d315909b86e203c5136a95530
Agnaldo Rozario <rozario.work@gmail.com>
2024-10-21 16:38:15 -0300

    Iniciando Projeto

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+
+        //dados do cliente
+        String nome = "Agnaldo Rozario";
+        String tipoConta = "Corrente";
+        double saldo = 2300;
+
+        //Inicia do programa com dados do cliente
+        System.out.println("*********************************");
+        System.out.println(" Nome: " + nome);
+        System.out.println("\n Tipo da conta: " + tipoConta);
+        System.out.println("\n Saldo atual: " + saldo);
+        System.out.println("*********************************\n");
+
+
+    }
+}
