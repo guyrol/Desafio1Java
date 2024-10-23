df2edcba5ffe1c836153dd2c4fbf10cbc400ff8c
Agnaldo Rozario <rozario.work@gmail.com>
2024-10-22 21:52:31 -0300

    1- Adicionado tratamento para entrada invalida do usuario, 2- Adicionado pause do sistema após função executada, 3-adicionado comentarios para documentação do código.

--- src/Main.java
+++ src/Main.java
@@ -31,19 +31,25 @@ public class Main {
         System.out.println("\n Saldo atual: " + saldo);
         System.out.println("*********************************\n");
 
-
+        //scanner para leitura de entrada do usuario
         Scanner leitura = new Scanner(System.in);
+        //scanner para pause no sistema
+        Scanner pause = new Scanner (System.in);
 
+        //laço de repetição para menu de funcionalidades
         do {
+            //imprime menu de opções
             System.out.println(menu);
-
             opcao = leitura.nextInt();
 
+            //Switch de opções do menu
             switch (opcao) {
                 case 1:
+                    //Verifica saldo
                     System.out.println("Saldo atual: " + saldo);
                     break;
                 case 2:
+                    //Deposito
                     System.out.println("Digite o valor que deseja depositar: ");
                     valor = leitura.nextDouble();
                     saldo += valor;
@@ -51,6 +57,7 @@ public class Main {
                     System.out.println("Saldo atual: " + saldo);
                     break;
                 case 3:
+                    //Saque
                     System.out.println("Digite o valor que deseja sacar:");
                     valor = leitura.nextDouble();
                     if (valor > saldo){
@@ -62,9 +69,17 @@ public class Main {
                     System.out.println("Saldo atual: " + saldo);
                     break;
                 case 4:
+                    //Sair
                     System.out.println("Finalizando");
                     break;
+                default:
+                    //Retorna opção invalida
+                    System.out.println("Opcão invalida");
+                    break;
             }
+            //pause do sistema
+            System.out.println("Prescione Enter para prosseguir.");
+            pause.nextLine();
 
         } while (opcao != 4);
 
